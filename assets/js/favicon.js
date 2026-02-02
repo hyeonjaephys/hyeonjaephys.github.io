db78eecccc61494d0e05891127c5f97469697190
Hyeonjae Kim <hyeonjae.amo@gmail.com>
2026-02-02 14:48:51 +0900

    Update favicon.js

--- assets/js/favicon.js
+++ assets/js/favicon.js
@@ -1,5 +1,5 @@
 const link = document.createElement("link");
 link.rel = "icon";
 link.type = "image/png";
-link.href = "/icons/favicon.png";
+link.href = "/assets/images/favicon.png";
 document.head.appendChild(link);
